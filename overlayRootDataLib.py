de49d17863a3483edfe4ce7c33b8dfa9dbf8bc10
jrb <>
2012-08-18 00:33:37 +0000

    support no-install headers on Windows

--- overlayRootDataLib.py
+++ overlayRootDataLib.py
@@ -2,7 +2,11 @@
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['overlayRootData'])
+        if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+	    env.Tool('findPkgPath', package = 'overlayRootData') 
     env.Tool('commonRootDataLib')
     env.Tool('facilitiesLib')
+    if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+        env.Tool('findPkgPath', package = 'enums') 
 def exists(env):
     return 1;
